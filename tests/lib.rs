e727c37e4f58e63d39226cfcc6fff98164a7dabe
Stephen Becker IV <github@deathbyescalator.com>
2015-07-28 10:24:52 -0700

    Move test file

--- tests/lib.rs
+++ tests/lib.rs
@@ -8,25 +8,24 @@ mod tests{
     use std::path::Path;
 
     fn read_sample_file(sample_name: &str) -> String{
-        let path = Path::new("../sample/"+sample_name);
+        let full_path = "../sample/".to_string()+sample_name;
+        let path = Path::new(&full_path);
         let mut file = match File::open(&path) {
-            Err(why) => panic!("couldn't open {}: {}", display,
+            Err(why) => panic!("couldn't open {}: {}", sample_name,
                                Error::description(&why)),
             Ok(file) => file,
         };
 
         // Read the file contents into a string, returns `io::Result<usize>`
         let mut s = String::new();
-        match file.read_to_string(&mut s) {
-            Err(why) => panic!("couldn't read {}: {}", display,
-                               Error::description(&why)),
-                               Ok(_) => print!("{} contains:\n{}", display, s),
-        }
+        file.read_to_string(&mut s).unwrap();
+        s
     }
     use warc_parser::*;
 
     #[test]
     fn it_parses_single(){
+        assert!(false);
         let bbc = "";
     }
 }
