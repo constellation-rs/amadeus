ac804724202e47eaa1ac283301b0fe5baef295a0
alecmocatta <alec@mocatta.net>
2020-07-18 11:43:46 +0100

    v0.3.4

--- amadeus-postgres/src/lib.rs
+++ amadeus-postgres/src/lib.rs
@@ -6,7 +6,7 @@
 //!
 //! This is a support crate of [Amadeus](https://github.com/constellation-rs/amadeus) and is not intended to be used directly. These types are re-exposed in [`amadeus::source`](https://docs.rs/amadeus/0.3/amadeus/source/index.html).
 
-#![doc(html_root_url = "https://docs.rs/amadeus-postgres/0.3.3")]
+#![doc(html_root_url = "https://docs.rs/amadeus-postgres/0.3.4")]
 #![cfg_attr(nightly, feature(type_alias_impl_trait))]
 #![warn(
 	// missing_copy_implementations,
